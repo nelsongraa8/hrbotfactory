8dae025ca6097c74223cd405e9a12c4c4278a458
Nelson R. Graña <87838742+nelsongraa8@users.noreply.github.com>
2022-03-27 20:04:55 +0000

    Test de toda la primera prueba

--- tests/SumaArrayTest.php
+++ tests/SumaArrayTest.php
@@ -13,6 +13,18 @@ class SumaArrayTest extends TestCase
         $this->methodArray($array, 8);
     }
 
+    public function testArraySubarray()
+    {
+        $array = [1, 2, [5, 8], 4];
+        $this->methodArray($array, 20);
+    }
+
+    public function testArraySubarrayNull()
+    {
+        $array = [0, [6, 2], null, 7, 1];
+        $this->methodArray($array, 16);
+    }
+
     private function methodArray($array, $resultArray)
     {
         $array = [1, 4, 3, 0];
